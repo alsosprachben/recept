ead57aa375732d016d4a3ba54808dfe424ccf88a
Ben Woolley <tautolog@gmail.com>
2019-04-29 19:25:55 -0700

    Add a frequency receptor.

--- recept.py
+++ recept.py
@@ -27,6 +27,17 @@ class Window:
 		self.i = (self.i + 1) % self.n
 		return v
 
+class CosineWindow(Window):
+	def __init__(self, period, phase):
+		from math import cos, pi
+		self.i = 0
+		self.n = period
+		self.w = [cos(2.0 * pi * ((float(i) + phase) / period)) for i in range(period)]
+
+class SineWindow(CosineWindow):
+	def __init__(self, period, phase):
+		CosineWindow.__init__(self, period, phase + (float(period) / 4))
+
 class PeriodicSmoothing:
 	def __init__(self, window = [1.0, -1.0], initial_value = 0.0):
 		self.w = WIndow(window)
@@ -35,10 +46,25 @@ class PeriodicSmoothing:
 	def sample(self, value):
 		return self.v.sample(value * self.w.next(), self.w.n)
 
-class FrequenchySmoothing:
-	def __init__(self, period, phase):
-		from math import cos, sin
-		
+class FrequencySmoothing:
+	def __init__(self, period, phase, real_initial_value = 0.0, imag_initial_value = 0.0):
+		self.real_w = CosineWindow(period, phase)
+		self.imag_w =   SineWindow(period, phase)
+		self.real_v = ExponentialSmoother(real_initial_value)
+		self.imag_v = ExponentialSmoother(imag_initial_value)
+
+	def sample(self, value):
+		return (
+			self.real_v.sample(value * self.real_w.next(), self.real_w.n)
+		+ 1j *	self.imag_v.sample(value * self.imag_w.next(), self.imag_w.n)
+		)
+
+	def sample_phase(self, value):
+		from cmath import phase
+		return phase(self.sample(value))
+
+	def sample_amplitude(Self, value):
+		return abs(self.sample(value))
 
 class SequenceDelta:
 	def __init__(self, prior_sequence = None):
@@ -81,6 +107,14 @@ class SmoothDuration:
 def liststr(l):
 	return ", ".join("%06.2f" % e for e in l)
 
+def listcompstr(l):
+	return ", ".join("%06.2f+%06.2fj" % (c.real, c.imag) for c in l)
+
+def listangstr(l):
+	from math import pi
+	from cmath import phase
+	return "".join("%06.2f = %06.2f (@ %03.2f)\n" % (f, abs(c), (phase(c) / (2.0 * pi)) % 1.0) for f, c in l)
+
 def main():
 	from sys import stdin, stdout
 	from time import time
@@ -107,8 +141,15 @@ def main():
 		for duration in range(1, 10)
 	]
 
+	freq_list = [
+		(duration, FrequencySmoothing(duration, 0))
+		for duration in range(1, 100)
+	]
+
 	n = None
+	frame = 0
 	while True:
+		frame += 1
 		line = stdin.readline()
 		t = time()
 		try:
@@ -123,10 +164,12 @@ def main():
 
 		results        = [sd.sample(n,  t) for sd in sd_list]
 		results_dev    = [sd.sample(abs(nd), t) for sd in dev_sd_list]
-		results_ratio  = [n/d for n, d in zip(results_dev, results)]
+		results_ratio  = [num/den for num, den in zip(results_dev, results)]
 		results_d1     = [sd_list_d1[i].sample(v) for i, v in enumerate(results)]
 		results_dev_d1 = [dev_sd_list_d1[i].sample(v) for i, v in enumerate(results_dev)]
-		stdout.write("event at time %.3f: %06.2f, %06.2f\n%r\n%r\n%r\n%r\n%r\n" % (t, n, nd, liststr(results), liststr(results_dev), liststr(results_ratio), liststr(results_d1), liststr(results_dev_d1)))
+		results_freq   = [(d, f.sample(n)) for d, f in freq_list]
+		
+		stdout.write("event at time %.3f frame %i: %06.2f, %06.2f\n%r\n%r\n%r\n%r\n%r\n%s\n" % (t, frame, n, nd, liststr(results), liststr(results_dev), liststr(results_ratio), liststr(results_d1), liststr(results_dev_d1), listangstr(results_freq)))
 		stdout.flush()
 
 if __name__ == "__main__":
