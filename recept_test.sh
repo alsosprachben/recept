4dbcfdef9e07cda2f90e4a2b1ce97e7fc2cd4413
Ben Woolley <tautolog@gmail.com>
2019-06-21 22:30:12 -0700

    recept_test.sh can now monitor any sox input file

--- recept_test.sh
+++ recept_test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sox test.wav --buffer 735 -r 44100 -c 1 -b 32 -e signed-integer -t raw - 2>/dev/null | pypy recept_mic.py  44100 735 1
+mkfifo sox.pipe
+sox "${1}" --buffer 735 -r 44100 -c 1 -b 32 -e signed-integer -t raw - 2>/dev/null | tee sox.pipe | play --buffer 735  -q -r 44100 -c 1 -b 32 -e signed-integer -t raw - &
+cat sox.pipe | pypy recept_mic.py  44100 735 10
