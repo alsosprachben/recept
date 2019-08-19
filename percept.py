c33439069b8ff1714c1c7a9cbf7a0b00da400659
Ben Woolley <tautolog@gmail.com>
2019-08-19 10:29:05 -0700

    make deviation a variable

--- percept.py
+++ percept.py
@@ -5,31 +5,32 @@ def main():
 	from time import sleep, time
 	from sys import stdout
 
-	transition = False
+	transition = True
 
+	dev = 1
 	while True:
 		for i in range(400):
-			x = random.normalvariate(-50, 1)
+			x = random.normalvariate(-50, dev)
 			sleep(random.expovariate(100.0))
 			stdout.write("%f\n" % x)
 			stdout.flush()
 
 		if transition:
 			for i in range(100):
-				x = random.normalvariate(-50 + i, 1)
+				x = random.normalvariate(-50 + i, dev)
 				sleep(random.expovariate(100.0))
 				stdout.write("%f\n" % x)
 				stdout.flush()
 
 		for i in range(400):
-			x = random.normalvariate(50, 1)
+			x = random.normalvariate(50, dev)
 			sleep(random.expovariate(100.0))
 			stdout.write("%f\n" % x)
 			stdout.flush()
 
 		if transition:
 			for i in range(100):
-				x = random.normalvariate(50 - i, 1)
+				x = random.normalvariate(50 - i, dev)
 				sleep(random.expovariate(100.0))
 				stdout.write("%f\n" % x)
 				stdout.flush()
