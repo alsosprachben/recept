eb90a6f2c8141222b591543da2a3a3b2f3aa7480
Ben Woolley <tautolog@gmail.com>
2019-09-10 00:16:26 -0700

    port tau.py to tau.h

---
+++ tau.h
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <complex.h>
+
+#define RADIAN_CYCLE (M_PI * 2)
+
+#define rad2tau(rad) (((((rad) / RADIAN_CYCLE) +0.5) % 1) - 0.5)
+#define tau2rad(tau)    ((tau) * RADIAN_CYCLE)
+
+#define rad_rect1(rad)      CMPLX(cos(rad),         sin(rad))
+#define rad_rect( rad, mag) CMPLX(cos(rad) * (mag), sin(rad) * (mag))
+#define rect1(    tau)      rad_rect1(      tau2rad(tau))
+#define rect(     tau, mag) rad_rect((mag), tau2rad(tau))
+
+#define rad_polar(cval) (cabs(cval),         carg(cval))
+#define polar(    cval) (cabs(cval), rad2tau(carg(cval)))
