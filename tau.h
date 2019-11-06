9839b447b4d7bab276ba6e7e387722a29f795633
Benjamin Woolley <ben@prie.st>
2019-11-06 11:53:53 -0800

    add CMPLX macros, for C99 compat

--- tau.h
+++ tau.h
@@ -4,6 +4,16 @@
 #include <math.h>
 #include <complex.h>
 
+#ifndef CMPLX
+#define CMPLX(x, y) ((double complex)((double)(x) + _Complex_I * (double)(y)))
+#endif
+#ifndef CMPLXF
+#define CMPLXF(x, y) ((float complex)((float)(x) + _Complex_I * (float)(y)))
+#endif
+#ifndef CMPLXL
+#define CMPLXL(x, y) ((long double complex)((long double)(x) + _Complex_I * (long double)(y)))
+#endif
+
 #define RADIAN_CYCLE (M_PI * 2)
 
 #define rad2tau(rad) (fmod(((rad) / RADIAN_CYCLE) + 0.5, 1) - 0.5)
