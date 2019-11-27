d34addae867fb1be00e5babc4bf78586ea02dede
Benjamin Woolley <ben@prie.st>
2019-11-27 12:44:04 -0800

    pass command line arguments through to the build command, to make it possible to pass `-lm` for building on OpenBSD and others which do not default libm.

--- recept_test_build.sh
+++ recept_test_build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cc -g -Ofast -flto -Wall -DRECEPT_TEST $@ bar.c screen.c sampler.c sampler_ui.c recept.c -o ./recept_test
+cc -g -Ofast -Wall -DRECEPT_TEST $@ bar.c screen.c sampler.c sampler_ui.c recept.c -o ./recept_test
